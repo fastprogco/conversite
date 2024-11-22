c7070c3fdc4a9ae143ff29c48972ad79c03b67af
Dyary Roof <dyary2020@gmail.com>
2024-11-23 01:24:42 +0300

    change file upload to publis folder

--- app/controllers/masters_controller.rb
+++ app/controllers/masters_controller.rb
@@ -6,12 +6,14 @@ class MastersController < ApplicationController
      def import_excel
        uploaded_file = params[:file]
        return redirect_to excel_import_show_masters_path, alert: 'Please select a file' if uploaded_file.nil?
-       
-       temp_file_path = Rails.root.join('tmp', uploaded_file.original_filename)
-       File.binwrite(temp_file_path, uploaded_file.read)
-       
-       MasterExcelImportJob.perform_later(temp_file_path.to_s)
-       redirect_to masters_path, notice: 'Data import is in progress'
+
+       file_path = Rails.root.join('public', 'uploads', uploaded_file.original_filename)
+       File.open(file_path, 'wb') do |file|
+         file.write(uploaded_file.read)
+       end
+
+       MasterExcelImportJob.perform_later(file_path.to_s)
+       redirect_to masters_path, notice: 'File uploaded successfully and data import is in progress'
      rescue StandardError => e
        redirect_to excel_import_show_masters_path, alert: "Error: #{e.message}"
      end
