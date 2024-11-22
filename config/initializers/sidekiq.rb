efdc02be3571687994a1c56573d74015bed51909
Dyary Roof <dyary2020@gmail.com>
2024-11-22 21:47:55 +0300

    use redis tls url instead

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -10,10 +10,10 @@ end
 
 if Rails.env.production?
     Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL']  }
+    config.redis = { url: ENV['REDIS_TLS_URL']  }
     end
 
     Sidekiq.configure_client do |config|
-    config.redis = { url:  ENV['REDIS_URL'] }
+    config.redis = { url:  ENV['REDIS_TLS_URL'] }
     end
 end
