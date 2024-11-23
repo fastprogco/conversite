87ecac5b42c1cfaa23d1447d5e59de1aa27c3086
Dyary Roof <dyary2020@gmail.com>
2024-11-23 03:29:31 +0300

    add new amazon s3 file upload

--- app/controllers/masters_controller.rb
+++ app/controllers/masters_controller.rb
@@ -6,13 +6,15 @@ class MastersController < ApplicationController
      def import_excel
        uploaded_file = params[:file]
        return redirect_to excel_import_show_masters_path, alert: 'Please select a file' if uploaded_file.nil?
-       
-       temp_file_path = Rails.root.join('tmp', uploaded_file.original_filename)
-       puts "writing file to #{temp_file_path}"
-       File.binwrite(temp_file_path, uploaded_file.read)
-       puts "file written to #{temp_file_path}"
-       
-       MasterExcelImportJob.perform_later(temp_file_path.to_s)
+
+       # Upload file to S3 using S3FileUploader service
+       begin
+        @file_url = S3FileUploader.upload(uploaded_file.tempfile.path, "dev")
+       rescue StandardError => e
+         return redirect_to excel_import_show_masters_path, alert: "Upload To S3 Error: #{e.message}"
+       end
+
+       MasterExcelImportJob.perform_later(@file_url)
        redirect_to masters_path, notice: 'Data import is in progress'
      rescue StandardError => e
        redirect_to excel_import_show_masters_path, alert: "Error: #{e.message}"
---
+++ app/services/s3_file_uploader.rb
@@ -0,0 +1,25 @@
+class S3FileUploader
+  def self.upload(file_path, environment)
+    # Extract the file name from the file path
+    file_name = File.basename(file_path)
+
+    # Generate a unique object key
+    timestamp = Time.now.strftime('%Y%m%d%H%M%S')
+    unique_id = SecureRandom.uuid
+    object_key = "uploads/#{timestamp}_#{unique_id}_#{file_name}"
+
+    # Retrieve the bucket name from credentials
+    bucket = Rails.application.credentials.dig(:aws, environment.to_sym, :bucket)
+
+    # Upload the file
+    File.open(file_path, 'rb') do |file|
+      S3_CLIENT.put_object(bucket: bucket, key: object_key, body: file)
+    end
+    
+    # Return the S3 file path
+    "https://#{bucket}.s3.amazonaws.com/#{object_key}"
+  rescue Aws::S3::Errors::ServiceError => e
+    puts "Upload Failed: #{e.message}"
+    nil
+  end
+end
