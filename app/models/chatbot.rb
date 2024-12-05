8dcfdfc6a89513cc0e320f07fa2c023465a3b089
Dyary Roof <dyary2020@gmail.com>
2024-12-05 22:04:35 +0300

    prevent more than one default chatbot

--- app/models/chatbot.rb
+++ app/models/chatbot.rb
@@ -9,4 +9,14 @@ class Chatbot < ApplicationRecord
     belongs_to :created_by, class_name: 'User', optional: true
     belongs_to :edited_by, class_name: 'User', optional: true
     belongs_to :deleted_by, class_name: 'User', optional: true
+
+    validate :only_one_default_chatbot
+
+    private
+
+    def only_one_default_chatbot
+        if master_segment_id.nil? && Chatbot.where(master_segment_id: nil).where.not(id: id).exists?
+            errors.add(:chatbot, "can only have one default chatbot without a segment")
+        end
+    end
 end
