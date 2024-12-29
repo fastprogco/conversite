c73080a48a1dd00549fffd24511358c85afaa93d
Dyary Roof <dyary2020@gmail.com>
2024-12-30 02:23:45 +0300

    filter by response_status in broadcast reposts work

--- app/controllers/broadcast_reports_controller.rb
+++ app/controllers/broadcast_reports_controller.rb
@@ -5,6 +5,8 @@ class BroadcastReportsController < ApplicationController
     @page = params[:page] || 1
     @broadcast = Broadcast.find(params[:broadcast_id])
     @message_status = params[:message_status] || ''
+    @response_status = params[:response_status] || ''
+    puts "response Status is here #{@response_status}"
     # @broadcast_reports = @broadcast.broadcast_reports.order(created_at: :desc).page(@page).per(10).map do |report|
     #   last_conversation = Conversation.where(mobile_number: report.mobile_number).order(created_at: :desc).first
     #   report.attributes.merge(
@@ -21,6 +23,7 @@ class BroadcastReportsController < ApplicationController
                                           ELSE 'Yes'
                                         END AS conversation_within_last_24_hours_of_report")
                               .where("message_status = :message_status OR :message_status_string = '' OR :message_status_string = 'all'", message_status: BroadcastReport.message_statuses[@message_status], message_status_string: @message_status)
+                              .where("(:response_status = 'yes' AND last_conversation.created_at IS NOT NULL AND last_conversation.created_at <= broadcast_reports.created_at + interval '24 hours') OR (:response_status = 'no' AND (last_conversation.created_at IS NULL OR last_conversation.created_at > broadcast_reports.created_at + interval '24 hours')) OR :response_status = ''", response_status: @response_status)
                               .order("broadcast_reports.created_at DESC")
                               .page(@page)
                               .per(10)
