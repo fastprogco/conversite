c90b46353cb0f9b54546d5097f0082ea3bc8f197
Dyary Roof <dyary2020@gmail.com>
2025-01-01 22:55:07 +0300

    fix bug in broadcast report response

--- app/controllers/broadcast_reports_controller.rb
+++ app/controllers/broadcast_reports_controller.rb
@@ -21,17 +21,17 @@ class BroadcastReportsController < ApplicationController
     # end
 
     @broadcast_reports = @broadcast.broadcast_reports
-                              .joins("LEFT JOIN LATERAL (SELECT * FROM conversations WHERE conversations.mobile_number = broadcast_reports.mobile ORDER BY created_at DESC LIMIT 1) last_conversation ON true")
+                              .joins("LEFT JOIN conversations ON conversations.mobile_number = broadcast_reports.mobile AND conversations.created_at < broadcast_reports.created_at + interval '24 hours'")
                               .joins("LEFT JOIN (SELECT mobile, created_at, id, master_segment_id FROM segments) segmentsx ON segmentsx.mobile = broadcast_reports.mobile AND segmentsx.created_at > broadcast_reports.created_at")
                               .joins("LEFT JOIN master_segments on master_segments.id = segmentsx.master_segment_id")
-                              .select("DISTINCT ON (broadcast_reports.*) broadcast_reports.*, segmentsx.id as segment_id,  master_segments.id as master_segment_id,
+                              .select("DISTINCT ON (broadcast_reports.id) broadcast_reports.*, segmentsx.id as segment_id,  master_segments.id as master_segment_id,
                                         CASE 
-                                          WHEN last_conversation.created_at IS NULL THEN 'No'
-                                          WHEN last_conversation.created_at < broadcast_reports.created_at + interval '24 hours' THEN 'YES'
+                                          WHEN conversations.created_at IS NULL THEN 'No'
+                                          WHEN conversations.created_at < broadcast_reports.created_at + interval '24 hours' THEN 'Yes'
                                           ELSE 'NO'
                                         END AS conversation_within_last_24_hours_of_report")
                               .where("message_status = :message_status OR :message_status_string = '' OR :message_status_string = 'all'", message_status: BroadcastReport.message_statuses[@message_status], message_status_string: @message_status)
-                              .where("(:response_status = 'yes' AND last_conversation.created_at IS NOT NULL AND last_conversation.created_at <= broadcast_reports.created_at + interval '24 hours') OR (:response_status = 'no' AND (last_conversation.created_at IS NULL OR last_conversation.created_at > broadcast_reports.created_at + interval '24 hours')) OR :response_status = ''", response_status: @response_status)
+                              .where("(:response_status = 'yes' AND conversations.created_at IS NOT NULL AND conversations.created_at <= broadcast_reports.created_at + interval '24 hours') OR (:response_status = 'no' AND (conversations.created_at IS NULL OR conversations.created_at > broadcast_reports.created_at + interval '24 hours')) OR :response_status = ''", response_status: @response_status)
                               
     if @trigger_segments == 'yes'
       @broadcast_reports  = @broadcast_reports.where("segmentsx.created_at > broadcast_reports.created_at")
