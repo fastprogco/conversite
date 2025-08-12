f39adc3376b0df9e425a01ea378f74ecb5725d98
Dyary Roof <dyary2020@gmail.com>
2025-08-12 23:09:52 +0300

    prevent duplicate whatsapp accounts

--- app/models/whatsapp_account.rb
+++ app/models/whatsapp_account.rb
@@ -2,6 +2,14 @@ class WhatsappAccount < ApplicationRecord
     belongs_to :added_by, class_name: "User"
     belongs_to :edited_by, class_name: "User", optional: true
     belongs_to :deleted_by, class_name: "User", optional: true
-
+    validates :whatsapp_mobile_number, 
+            :app_id, 
+            :phone_number_id, 
+            :whatsapp_business_account_id, 
+            :token, 
+            uniqueness: true
     validates :name, :whatsapp_mobile_number, :app_id, :phone_number_id, :whatsapp_business_account_id, :token, :webhook_version, presence: true
+    validates :added_by, 
+          uniqueness: { conditions: -> { where(is_deleted: false) }, 
+                        message: "already has a WhatsApp account" }
 end
